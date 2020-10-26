06d970285142511923b748dcd55683ed9b862547
Stefan Crummenerl <crummenerls@gmail.com>
2020-10-26 21:20:03 +0100

    cs50 pset4 recover final version

--- pset4/recover/recover.c
+++ pset4/recover/recover.c
@@ -3,30 +3,38 @@
 #include <stdbool.h>
 #include <stdint.h>
 
-typedef uint8_t BYTE;
-
 #define BLOCKSIZE 512
 
-bool isJpg(BYTE buffer[]);
+bool isJpg(uint8_t buffer[]);
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-    {
-        printf("Usage: ./recover image\n");
-        return 1;
-    }
-    FILE *inputFile = fopen(argv[1], "r");
-    if (inputFile == NULL)
-    {
-        printf("File not found\n");
-        return 1;
-    }
+    // 1 Check argc
+    // 2 Open file and check if is != NULL && create outputFile;
+    // 3 Create buffer, jpgCount, bool flag if is 1st jpg and filename holder;
+    // 4 fread in
+    //   4.1 check if isJpg
+    //     4.1.1 check if not 1st jpg
+    //       4.1.1.1 set is 1st to true
+    //       4.1.1.1 else close outputFile
+    //     4.1.2 send formatted output to a string pointed to, by str && increase jpg count
+    //           we gona write in here
+    //     4.1.3 open output file ... and check it ...
+    //     4.1.4 write buffer inside
+    //   4.1 else if is 1st jpg write buffer inside
 
-    BYTE buffer[BLOCKSIZE];
-    int file_index = 0;
+    if (argc != 2)
+        return 1;
+
+    FILE *inputFile = fopen(argv[1], "r");
+    FILE *outputFile = NULL;
+    if (inputFile == NULL)
+        return 1;
+
+    uint8_t buffer[BLOCKSIZE];
+    int jpgCount = 0;
     bool is_1st_jpg = false;
-    FILE *outputFile;
+    char filename[8];
 
     while (fread(buffer, BLOCKSIZE, 1, inputFile))
     {
@@ -37,8 +45,7 @@ int main(int argc, char *argv[])
             else
                 fclose(outputFile);
 
-            char filename[8];
-            sprintf(filename, "%03i.jpg", file_index++);
+            sprintf(filename, "%03i.jpg", jpgCount++);
             outputFile = fopen(filename, "w");
             if (outputFile == NULL)
                 return 1;
