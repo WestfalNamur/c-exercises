944f6304164cfbcc51354990c9775b37a0ea2c36
Stefan Crummenerl <crummenerls@gmail.com>
2020-10-31 12:31:45 +0100

    cs50 substitution still on it ..

--- cs50/pset2/substitution.c
+++ cs50/pset2/substitution.c
@@ -11,9 +11,15 @@ int main(int argc, char *argv[])
 {
     // ************************************************************************
     if (argc != 2)
+    {
+        printf("No key!");
         return 1;
+    }
     if (strlen(argv[1]) != 26)
+    {
+        printf("key is not 26 chars long");
         return 1;
+    }
     for (int i = 0; i < 26; i++)
     {
         char c = ABC[i];
@@ -24,18 +30,40 @@ int main(int argc, char *argv[])
                 c_in_key = true;
         }
         if (c_in_key == false)
+        {
+            printf("%c not in ABC", c);
             return 1;
+        }
     }
 
     // ************************************************************************
     string msg = get_string("Input: ");
     int msg_upper_012[strlen(msg)];
+    char key[strlen(argv[1])];
     char msg_upper[strlen(msg)];
     char msg_upper_enrypted[strlen(msg)];
     char msg_enrypted[strlen(msg)];
 
     printf("plaintext: ");
     prnt_str(msg);
+
+    // ************************************************************************
+    // KEY TO UPPER
+    for (int i = 0, n = strlen(argv[1]); i < n; i++)
+    {
+        int ci = (int)argv[1][i];
+        char c;
+        if (ci >= 65 && ci <= 90)
+            c = ci;
+        else if (ci >= 97 && ci <= 122)
+            c = ci - 32;
+        else
+            printf("%c is not an ABC char!", ci);
+        key[i] = (char)c;
+    }
+
+    printf("key: ");
+    prnt_str(key);
 
     // ************************************************************************
     //  TO UPPER
@@ -58,8 +86,8 @@ int main(int argc, char *argv[])
             msg_upper_012[i] = 2;
         }
     }
-    printf("plaintext upper: ");
-    prnt_str(msg_upper);
+    //printf("plaintext upper: ");
+    //prnt_str(msg_upper);
 
     // ************************************************************************
     // ENCRYPT UPPER
@@ -70,33 +98,30 @@ int main(int argc, char *argv[])
         {
             int x = argv[1][c - 65];
             msg_upper_enrypted[i] = (char)x;
-            printf("In: %c  Out: %c \n", c, msg_upper_enrypted[i]);
+            // printf("In: %c  Out: %c \n", c, msg_upper_enrypted[i]);
         }
         else
         {
             msg_upper_enrypted[i] = (char)c;
-            printf("In: %c  Out: %c \n", c, msg_upper_enrypted[i]);
+            // printf("In: %c  Out: %c \n", c, msg_upper_enrypted[i]);
         }
     }
-    printf("encrypted upper: ");
-    prnt_str(msg_upper_enrypted);
+    // printf("encrypted upper: ");
+    // prnt_str(msg_upper_enrypted);
 
     // ************************************************************************
-    // CASING
-    for (int i = 0, n = strlen(msg); i < n; i++)
+    // ADD CASING back
+    for (int i = 0, n = strlen(msg_upper_enrypted); i < n; i++)
     {
-        if (msg_upper_012[i] == 0)
-        {
-            int c = msg_upper_enrypted[i];
-            int x = c + 32;
-            msg_enrypted[i] = (char)x;
-        }
+        int u = msg_upper_012[i];
+        int c;
+        if (u == 0)
+            c = (int)msg_upper_enrypted[i] + 32;
         else
-        {
-            msg_enrypted[i] = msg_upper_enrypted[i];
-        }
+            c = (int)msg_upper_enrypted[i];
+        msg_enrypted[i] = (char)c;
     }
-    printf("encrypted: ");
+    printf("ciphertext: ");
     prnt_str(msg_enrypted);
 
     // ************************************************************************
